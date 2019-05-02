f6b5753f0513dc2f868a5f02a4bec5737ede3ad3
Greg Kaestle <flagscript@gmail.com>
2019-05-02 06:00:37 -0700

    Adding integration tests

---
+++ test/Flagscript.Gravatar.Integration.Tests/GravatarTestFixture.cs
@@ -0,0 +1,28 @@
+﻿namespace Flagscript.Gravatar.Integration.Tests
+{
+
+	/// <summary>
+	/// Test Fixture for the various gravatar unit tests.
+	/// </summary>
+	public class GravatarTestFixture
+	{
+
+		/// <summary>
+		/// <see cref="GravatarLibrary"/> to use during testing.
+		/// </summary>
+		/// <value><see cref="GravatarLibrary"/> to use during testing</value>
+		public GravatarLibrary Library { get; private set; }
+
+		/// <summary>
+		/// Contructor initializing test fixture.
+		/// </summary>
+		public GravatarTestFixture()
+		{
+
+			Library = new GravatarLibrary();
+
+		}
+
+	}
+
+}
